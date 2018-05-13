d0de08abc54ccd2c86e37dcd41d7bae8db747b7a
Sarkar <aditya.sarkar@accenture.com>
2018-05-14 00:25:18 +0530

    form processing changes

--- submit.php
+++ submit.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
+$connection = mysql_connect("localhost", "adityas3_adi", "itsmyname@28"); // Establishing Connection with Server..
 $db = mysql_select_db("adityas3_myForm", $connection); // Selecting Database
 //Fetching Values from URL
-$msg2=$_POST['msg1'];
 $email2=$_POST['email1'];
+$msg2=$_POST['msg1'];
 //Insert query
 $query = mysql_query("insert into tblUserInfo(email, msg) values ('$email2','$msg2')");
 echo "Form Submitted Succesfully";
